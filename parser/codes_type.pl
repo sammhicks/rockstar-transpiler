76be311be5e2a1541a144d60160f3313064d9061
Samuel Hicks <sh14820@my.bristol.ac.uk>
2018-07-22 14:40:40 +0100

    now parsing proper variables and pronouns

--- parser/codes_type.pl
+++ parser/codes_type.pl
@@ -1,14 +1,25 @@
 :- module(codes_type, [
-	      codes_type//2
+	      string_type//2,
+	      codes_type//3,
+	      code_type//2
 	  ]).
 
-codes_type(String, Type, Codes, Rest) :-
-	codes_type_inner(Codes, Rest, Type, String_Codes),
+string_type(String, Type, Codes, Rest) :-
+	codes_type(String_Codes, [], Type, Codes, Rest),
 	string_codes(String, String_Codes).
 
-codes_type_inner([Code|Codes], Rest, Type, [Code|String_Codes]) :-
+
+codes_type(Matched_Codes, Matched_Rest, Type, Codes, Rest) :-
+	codes_type_inner(Codes, Rest, Type, Matched_Codes, Matched_Rest).
+
+
+codes_type_inner([Code|Codes], Rest, Type, [Code|String_Codes], Matched_Rest) :-
 	code_type(Code, Type),
 	!,
-	codes_type_inner(Codes, Rest, Type, String_Codes).
+	codes_type_inner(Codes, Rest, Type, String_Codes, Matched_Rest).
 
-codes_type_inner(Rest, Rest, _, []).
+codes_type_inner(Rest, Rest, _, Matched_Rest, Matched_Rest).
+
+
+code_type(Code, Type, [Code|Rest], Rest) :-
+	code_type(Code, Type).
--- parser/variables.pl
+++ parser/variables.pl
@@ -1,15 +1,28 @@
 
 :- module(variables, [
-	      common_variable//1
+	      variable//1
 	  ]).
 
 :- use_module(codes_type).
+
+
+variable(Name) -->
+	common_variable(Name),
+	!.
+
+variable(Name) -->
+	proper_variable(Name),
+	!.
+
+variable(pronoun) -->
+	pronoun.
+
 
 common_variable(Name) -->
 	common_variable_prefix,
 	" ",
 	!,
-	codes_type(Name, lower).
+	string_type(Name, lower).
 
 
 common_variable_prefix -->
@@ -21,3 +34,60 @@ common_variable_prefix -->
 common_variable_prefix -->
 	"your".
 
+
+proper_variable(Name, Codes, Rest) :-
+	proper_variable_codes(Name_Codes, Codes, Rest),
+	string_codes(Name, Name_Codes).
+
+
+proper_variable_codes(Name) -->
+	proper_variable_word(Name, Name_Tail),
+	proper_variable_tail(Name_Tail).
+
+
+proper_variable_tail(Words) -->
+	proper_variable_tail(Words, []).
+
+
+proper_variable_tail(Words, Rest) -->
+	proper_variable_tail_word(Words, Tail),
+	!,
+	proper_variable_tail(Tail, Rest).
+
+proper_variable_tail(Rest, Rest) -->
+	[].
+
+
+proper_variable_tail_word([Space|Letters], Rest) -->
+	" ",
+	proper_variable_word(Letters, Rest),
+	{
+	    string_codes(" ", [Space])
+	}.
+
+
+proper_variable_word([Letter|Letters], Rest) -->
+	code_type(Letter, upper),
+	codes_type(Letters, Rest, lower).
+
+
+pronoun -->
+	"it".
+
+pronoun -->
+	"he".
+
+pronoun -->
+	"she".
+
+pronoun -->
+	"him".
+
+pronoun -->
+	"her".
+
+pronoun -->
+	"them".
+
+pronoun -->
+	"they".
